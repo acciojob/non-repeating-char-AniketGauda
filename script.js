ec4ad78ab6cd501c3d1f7d4e0c53716f9ce0af31
Aniket Gauda <122367767+AniketGauda@users.noreply.github.com>
2024-05-19 10:16:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let obj = {};
+    for(let i=0;i<str.length;i++){
+        let ch = str[i];
+        if(ch in obj)
+            obj[ch]++;
+        else
+            obj[ch] = 1;
+    }
+    let ans=null;
+    for(let key in obj){
+        if(obj[key]==1){
+            ans=key;
+        }
+    }
+
+    return ans;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
